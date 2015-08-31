1462d57cd5a3b6e92f36bfea51a14ca0a4e15345
Sunny <kpsingh0801@gmail.com>
2015-08-31 18:59:26 +0530

    validation on email

--- app/controllers/api/v1/email_controller.rb
+++ app/controllers/api/v1/email_controller.rb
@@ -4,12 +4,24 @@ class Api::V1::EmailController < ApplicationController
   before_filter :authenticate_from_user_token
 
   def create
-    UserMailer.api params[:email]
-    render :nothing => true
+    if valid_email? 
+      UserMailer.api params[:email]
+      render :nothing => true
+    else
+      render json: {status: :email_not_valid}
+    end
   end
   
-  def authenticate_from_user_token
-    token = User.where(email: "admin@example.com").last.try(:authentication_token)
-    render json: {status: :unauthorized} if params[:token] != token
-  end
+  private 
+  
+    def valid_email?
+        valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+        params[:email][:to] =~ valid_email_regex
+      end
+    
+    def authenticate_from_user_token
+        token = User.where(email: "admin@example.com").last.try(:authentication_token)
+        render json: {status: :unauthorized} if params[:token] != token
+    end
 end
+
